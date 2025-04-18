ed3c89a91c4ae43076e70bda564c56dd19363a50
Kirill Zolotaryov <252222223a@gmail.com>
2025-04-18 21:31:27 +0300

    Адаптировал под мобилку карточку психолога

--- src/widgets/Card/Card.tsx
+++ src/widgets/Card/Card.tsx
@@ -118,22 +118,14 @@ export const Card:React.FC<Props> = ({data}) => {
                                     <span className="text-[#9A9A9A] flex flex-col gap-[10px] font-normal text-[16px] leading-[22px] w-full max-lg:text-[14px] max-lg:mt-[5px]">
                                         Ближайшая запись:
                                         
-                                        <ul className="flex gap-[10px] mt-[5px] overflow-auto min-w-full">
-                                            <li className="max-lg:text-[14px] relative shrink-0 rounded-[50px] w-[132px]  border-[1px] border-[#D4D4D4]  text-[#116466] font-normal leading-[25px] text-[18px]  flex justify-center items-center">
-                                                <button className="relative h-full w-full cursor-pointer p-[8px] py-[8px]">
-                                                    28.01/ 13:00
-                                                </button> 
+                                        <ul className="flex gap-[10px] overflow-x-auto pb-2">
+                                        {[1, 2, 3].map((_, i) => (
+                                            <li key={i} className="shrink-0 rounded-[50px] w-[132px] max-lg:w-[109px] max-lg:text-[14px] border-[1px] border-[#D4D4D4] text-[#116466] font-normal leading-[25px] text-[18px] flex justify-center items-center">
+                                            <button className="w-full h-full py-[8px] px-[12px]">
+                                                28.01/ 13:00
+                                            </button>
                                             </li>
-                                            <li className="max-lg:text-[14px] relative shrink-0 rounded-[50px] w-[132px border-[1px] border-[#D4D4D4]  text-[#116466] font-normal leading-[25px] text-[18px] flex justify-center items-center">
-                                                <button className="relative h-full w-full cursor-pointer p-[8px] py-[8px]">
-                                                    28.01/ 13:00
-                                                </button> 
-                                            </li>
-                                            <li className="max-lg:text-[14px] relative shrink-0 rounded-[50px] w-[132px] border-[1px] border-[#D4D4D4]  text-[#116466] font-normal leading-[25px] text-[18px] flex justify-center items-center">
-                                                <button className="relative h-full w-full cursor-pointer p-[8px] py-[8px]">
-                                                    28.01/ 13:00
-                                                </button> 
-                                            </li>
+                                        ))}
                                         </ul>
                                     </span>
                                 </div>
@@ -144,18 +136,45 @@ export const Card:React.FC<Props> = ({data}) => {
                             <span className="text-[#9A9A9A] font-normal text-[16px] leading-[22px] max-lg:text-[14px]">
                                 Запросы:
                             </span>   
-                            <ul className="  min-w-fit gap-[10px] max-[425px]:gap-x-[120px]  grid-cols-[repeat(auto-full,_minmax(100px,_1fr))] w-initial mt-[5px] flex-wrap max-lg:grid-cols-[repeat(2,_minmax(100px,_1fr))] inline-grid w-fit overflow-auto">
-                                {
-                                    queries.map((item, i ) => <li key={i} className="max-lg:text-[14px] h-fit min-w-[200px] shrink-0 rounded-[20px] font  max-lg:max-w-[300px] text-[18px] font-semibold w-fit  border-[1px] border-[#D4D4D4]  text-[#116466] leading-[25px] ">
-                                        <button className="relative h-full w-full cursor-pointer p-[8px] flex justify-start">
-                                        {
-                                            item
-                                        }
-                                        </button>    
-                                    </li>)
-                                }
-                                
+                            <div className="w-full">
+                            {/* Мобильная версия (горизонтальный скролл) */}
+                            <div className="lg:hidden overflow-x-auto pb-2 hide-scrollbar">
+                                <ul className="flex gap-[10px] mt-[5px] w-max">
+                                {queries.map((item, i) => (
+                                    <li 
+                                    key={i}
+                                    className="
+                                        min-w-[200px] text-[14px]
+                                        rounded-[20px] border-[1px] border-[#D4D4D4]
+                                        text-[#116466] font-semibold leading-[25px]
+                                    "
+                                    >
+                                    <button className="w-full h-full p-[8px] text-left whitespace-normal">
+                                        {item}
+                                    </button>
+                                    </li>
+                                ))}
+                                </ul>
+                            </div>
+
+                            {/* Десктоп версия (2 колонки) */}
+                            <ul className="hidden lg:grid grid-cols-2 gap-[10px] mt-[5px]">
+                                {queries.map((item, i) => (
+                                <li 
+                                    key={i}
+                                    className="
+                                    text-[18px] rounded-[20px]
+                                    border-[1px] border-[#D4D4D4]
+                                    text-[#116466] font-semibold leading-[25px]
+                                    "
+                                >
+                                    <button className="w-full h-full p-[8px] text-left whitespace-normal">
+                                    {item}
+                                    </button>
+                                </li>
+                                ))}
                             </ul>
+                            </div>
                         </div>
                     </div>
                 
@@ -254,65 +273,48 @@ export const Card:React.FC<Props> = ({data}) => {
                             </ul>       
                         </div>
 
-                        <ul className="mt-[30px] w-fit flex grid-cols-4 font-normal text-[18px] leading-[25px] gap-[30px] max-[425px]:grid-cols-2 max-[425px]:gap-[30px]">
-                            <li>
-                                <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
-                                    Личная терапия:
-                                </span>
-                                
-                                <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px]">
-                                    Да
-                                    <Image src={'/card/hint.svg'} alt="hint" height={23} width={23} />
 
-                                </span>
-                            </li>
+                        <ul className="mt-[30px] flex flex-wrap gap-x-[30px] gap-y-[20px] font-normal text-[18px] leading-[25px] max-lg:grid max-lg:grid-cols-2">
+                        <li className="flex flex-col items-start whitespace-nowrap w-fit">
+                            <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
+                            Личная терапия:
+                            </span>
+                            <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px] items-center">
+                            Да
+                            <Image src={'/card/hint.svg'} alt="hint" height={23} width={23} />
+                            </span>
+                        </li>
 
-                            <li className="w-[200px]">
-                                <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
-                                Посещает супервизию:
-                                </span>
-                                
-                                <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px]">
-                                    Да
-                                    <Image src={'/card/hint.svg'} alt="hint" height={23} width={23} />
+                        <li className="flex flex-col items-start whitespace-nowrap w-fit">
+                            <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
+                            Посещает супервизию:
+                            </span>
+                            <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px] items-center">
+                            Да
+                            <Image src={'/card/hint.svg'} alt="hint" height={23} width={23} />
+                            </span>
+                        </li>
 
-                                </span>
-                            </li>
-                            <li>
-                                <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
-                                    Семейное положение
-                                </span>
-                                
-                                <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px]">
-                                    {
-                                            data.sex === 'Мужчина' &&
-                                            (data.is_married && data.has_children) && 'Женат, дети' 
-                                    }
-                                    {
-                                            data.sex === 'Женщина' &&
-                                            (data.is_married && data.has_children) && 'Замужем, дети' 
-                                    }
-                                    {
-                                            data.sex === 'Мужчина' &&
-                                            (data.is_married === false) && 'Не женат' 
-                                    }
-                                    {
-                                            data.sex === 'Женщина' &&
-                                            (data.is_married === false) && 'Не женат' 
-                                    }
-                                </span>
-                            </li>
-                            <li>
-                                <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
-                                    Есть дети:
-                                </span>
-                                
-                                <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px]">
-                                    {
-                                        data.has_children ? 'Да' : 'Нет'
-                                    }
-                                </span>
-                            </li>
+                        <li className="flex flex-col items-start whitespace-nowrap w-fit">
+                            <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
+                            Семейное положение:
+                            </span>
+                            <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px] items-center">
+                            {data.sex === 'Мужчина' && (data.is_married && data.has_children) && 'Женат, дети'}
+                            {data.sex === 'Женщина' && (data.is_married && data.has_children) && 'Замужем, дети'}
+                            {data.sex === 'Мужчина' && (data.is_married === false) && 'Не женат'}
+                            {data.sex === 'Женщина' && (data.is_married === false) && 'Не замужем'}
+                            </span>
+                        </li>
+
+                        <li className="flex flex-col items-start whitespace-nowrap w-fit">
+                            <span className="text-[#9A9A9A] text-[16px] leading-[22px] max-lg:text-[12px]">
+                            Есть дети:
+                            </span>
+                            <span className="flex text-[#151515] gap-[10px] mt-[10px] max-lg:text-[14px] items-center">
+                            {data.has_children ? 'Да' : 'Нет'}
+                            </span>
+                        </li>
                         </ul>
                     </div> 
                 }
