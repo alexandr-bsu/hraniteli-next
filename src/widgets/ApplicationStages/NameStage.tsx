6e10f456d4fe54a84cbf80e7a745e1a7c4e4e61f
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2025-05-19 19:31:24 +0800

    fix fonts

--- src/widgets/ApplicationStages/NameStage.tsx
+++ src/widgets/ApplicationStages/NameStage.tsx
@@ -55,7 +55,7 @@ export default function NameStageApplication() {
                   <FormLabel className='text-[20px] lg:text-[20px] md:text-[16px] max-lg:text-[14px] leading-[27px] max-lg:leading-[22px] font-semibold'>
                     Как вас зовут?
                   </FormLabel>
-                  <FormDescription className='text-[14px] leading-[20px] font-normal max-lg:mt-[8px]'>
+                  <FormDescription className='text-[18px] leading-[20px] font-normal max-lg:mt-[8px]'>
                     Вы можете не указывать имя, если пока не готовы
                   </FormDescription>
                   <div className={styles.input__text_container}>
