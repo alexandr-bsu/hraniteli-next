d7023d04c3b0475887688ca3c8cbef2beba896e5
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2025-07-03 16:08:59 +0800

    remove 300 rub item

--- src/widgets/HelpHandStages/PriceSessionStage.tsx
+++ src/widgets/HelpHandStages/PriceSessionStage.tsx
@@ -99,15 +99,6 @@ export const PriceSessionStage = () => {
 
                                             <FormItem className="flex items-center gap-[15px] max-lg:gap-[12px]">
                                                 <FormControl>
-                                                    <RadioGroupItem className="h-[30px] w-[30px] max-lg:h-[24px] max-lg:w-[24px]" value="300" />
-                                                </FormControl>
-                                                <FormLabel className={`text-[18px] lg:text-[18px] md:text-[14px] max-lg:text-[14px] leading-[25px] max-lg:leading-[20px] font-normal text-[${COLORS.text.primary}]`}>
-                                                    300 руб
-                                                </FormLabel>
-                                            </FormItem>
-
-                                            <FormItem className="flex items-center gap-[15px] max-lg:gap-[12px]">
-                                                <FormControl>
                                                     <RadioGroupItem className="h-[30px] w-[30px] max-lg:h-[24px] max-lg:w-[24px]" value="500" />
                                                 </FormControl>
                                                 <FormLabel className={`text-[18px] lg:text-[18px] md:text-[14px] max-lg:text-[14px] leading-[25px] max-lg:leading-[20px] font-normal text-[${COLORS.text.primary}]`}>
