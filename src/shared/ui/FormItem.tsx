a447209f4a2b84f7c71014b662967c51ff918eb2
Alex <rodionov.alexandr-sergeevich@yandex.ru>
2025-10-20 22:31:24 +0800

    increase text size

--- src/shared/ui/FormItem.tsx
+++ src/shared/ui/FormItem.tsx
@@ -5,7 +5,7 @@ const FormItem = ({ children, title, description }: { children: React.ReactNode,
   return (
     <div className='grow'>
                 <div className='grow p-[30px] max-lg:max-h-none max-lg:p-[15px] border-[1px] border-border rounded-[25px]'>
-                    <h2 className='text-[20px] lg:text-[20px] md:text-[14px] max-lg:text-[14px] leading-[27px] max-lg:leading-[22px] font-semibold'>
+                    <h2 className='text-[20px] lg:text-[20px] md:text-[16px] max-lg:text-[16px] leading-[27px] max-lg:leading-[22px] font-semibold'>
                         {title}
                     </h2>
                     {description && <p className='text-secondary-foreground text-[18px] lg:text-[18px] mt-1 md:text-[14px] max-lg:text-[14px] leading-[25px] max-lg:leading-[20px] font-normal'>
