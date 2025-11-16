f741a6f220925b713be1c3530de50495c39e12bf
Alex <rodionov.alexandr-sergeevich@yandex.ru>
2025-11-16 17:43:55 +0800

    change z-index close modal button

--- src/widgets/ModalWindow/ModalWindow.tsx
+++ src/widgets/ModalWindow/ModalWindow.tsx
@@ -26,7 +26,7 @@ export const ModalWindow: React.FC<Props> = ({ onOpenChange, className, maxWidth
                 open={open !== undefined ? open : (isOpen && modalType === type)}>
                 <DialogContent maxWidth={maxWidth} className={`w-[95%] max-w-[640px] p-[30px] rounded-[30px] ${className}`}>
                     {
-                        closeButton && <DialogClose className="w-[40px] h-[40px] shrink-0 flex justify-center items-center border-2 border-[#D4D4D4]  ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 rounded-full">
+                        closeButton && <DialogClose className="w-[40px] h-[40px] shrink-0 flex justify-center items-center border-2 border-[#D4D4D4] ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 rounded-full z-[9999]">
                             <Image src={'/modal/cross.svg'} alt="cross" height={15} width={15} />
                         </DialogClose>
                     }
